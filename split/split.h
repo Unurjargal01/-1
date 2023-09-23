d29df7ebff27289bf67f35bc1ca71bd2d22b05de
Unurjargal01 <ugantumur@edu.hse.ru>
2023-09-23 16:38:56 +0300

    first attempt

--- split/split.h
+++ split/split.h
@@ -5,5 +5,16 @@
 #include <vector>
 
 std::vector<std::string> Split(const std::string& string, const std::string& delimiter = " ") {
-    throw std::runtime_error{"Not implemented"};
+    std::vector<std::string> ans;
+    std::string::size_type prev = 0, cur = string.find(delimiter);
+    if (string.empty()) {
+        return ans;
+    }
+    while (cur != std::string::npos) {
+        ans.push_back(string.substr(prev, cur - prev));
+        prev = cur + delimiter.size();
+        cur = string.find(delimiter, prev);
+    }
+    ans.push_back(string.substr(prev, string.size()));
+    return ans;
 }
