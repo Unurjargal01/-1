9325be0a2b6e71d176fda3d6084bd2fc0222fee3
Unurjargal01 <ugantumur@edu.hse.ru>
2023-09-23 17:42:24 +0300

    word_count

--- word-count/word_count.h
+++ word-count/word_count.h
@@ -2,7 +2,23 @@
 
 #include <string>
 #include <stdexcept>
+#include <unordered_set>
+#include <ctype.h>
 
 int DifferentWordsCount(const std::string& string) {
-    throw std::runtime_error{"Not implemented"};
+    std::unordered_set<std::string> words;
+    std::string word;
+    for (auto c : string) {
+        c = std::tolower(c);
+        if (std::isalpha(c)) {
+            word.push_back(c);
+        } else if (!word.empty()) {
+            words.insert(word);
+            word = "";
+        }
+    }
+    if (!word.empty()) {
+        words.insert(word);
+    }
+    return words.size();
 }
