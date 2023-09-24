aa305eb3f5439c6e664fa8861d135508efbde4f6
Unurjargal01 <ugantumur@edu.hse.ru>
2023-09-24 17:13:06 +0300

    Attempt 1: Using Map

--- diff-pairs/diff_pairs.h
+++ diff-pairs/diff_pairs.h
@@ -3,7 +3,19 @@
 #include <vector>
 #include <cstdint>
 #include <stdexcept>
+#include <unordered_map>
+#include <map>
 
 int64_t CountPairs(const std::vector<int>& data, int x) {
-    throw std::runtime_error{"Not implemented"};
+    std::map<int, int> counter;
+    int64_t cnt = 0;
+    for (const int& v : data) {
+        if (counter.find(x - v) != counter.end()) {
+            cnt += counter[x - v];
+        }
+        if (counter.insert(std::pair<int, int>(v, 1)).second == 0) {
+            counter[v] += 1;
+        };
+    }
+    return cnt;
 }
