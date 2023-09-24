04070754ab3e15e56c85b68bc9e963d8ac7dc677
Unurjargal01 <ugantumur@edu.hse.ru>
2023-09-24 17:27:29 +0300

    Attempt 2: Tried to fixed |x - v| < 2**31

--- diff-pairs/diff_pairs.h
+++ diff-pairs/diff_pairs.h
@@ -5,17 +5,21 @@
 #include <stdexcept>
 #include <unordered_map>
 #include <map>
+#include <cmath>
 
 int64_t CountPairs(const std::vector<int>& data, int x) {
     std::map<int, int> counter;
     int64_t cnt = 0;
     for (const int& v : data) {
-        if (counter.find(x - v) != counter.end()) {
-            cnt += counter[x - v];
+        int64_t dif = x - v;
+        if (std::abs(dif) <= std::pow(2, 31) - 1) {
+            if (counter.find(x - v) != counter.end()) {
+                cnt += counter[x - v];
+            }
+            if (counter.insert(std::pair<int, int>(v, 1)).second == 0) {
+                counter[v] += 1;
+            }
         }
-        if (counter.insert(std::pair<int, int>(v, 1)).second == 0) {
-            counter[v] += 1;
-        };
     }
     return cnt;
 }
