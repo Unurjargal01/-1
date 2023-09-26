bec011cf0e19bd143b8a4280ab7421df40741a69
Unurjargal01 <ugantumur@edu.hse.ru>
2023-09-27 00:53:35 +0300

    Long_Sum Attempt: 1

--- long-sum/long_sum.h
+++ long-sum/long_sum.h
@@ -2,7 +2,42 @@
 
 #include <string>
 #include <stdexcept>
+#include <algorithm>
 
 std::string LongSum(const std::string& a, const std::string& b) {
-    throw std::runtime_error{"Not implemented"};
+    int a_size = a.size() - 1, b_size = b.size() - 1, rem = 0;
+    std::string ans;
+    if (a.empty()) {
+        return b;
+    } else if (b.empty()) {
+        return a;
+    }
+    while (a_size >= 0 && b_size >= 0) {
+        char c = a[a_size] + b[b_size] - 48 + rem;
+        rem = 0;
+        if (c >= 58) {
+            ++rem;
+            c -= 10;
+        }
+        ans.push_back(c);
+        --a_size;
+        --b_size;
+    }
+
+    std::reverse(ans.begin(), ans.end());
+    std::string ans1;
+    std::string ch;
+    if (rem > 0) {
+        ch = "1";
+    }
+    if (a_size >= 0) {
+        ans1 = a.substr(0, a_size + 1);
+    } else if (b_size >= 0) {
+        ans1 = b.substr(0, b_size + 1);
+    }
+
+    ans1 = LongSum(ans1, ch);
+    ans1 += ans;
+
+    return ans1;
 }
