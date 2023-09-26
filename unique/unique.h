e0cb8b8766406256750a4aaf965e6cbf25239a1f
Unurjargal01 <ugantumur@edu.hse.ru>
2023-09-26 21:40:10 +0300

    Unique with std::map

--- unique/unique.h
+++ unique/unique.h
@@ -2,7 +2,17 @@
 
 #include <vector>
 #include <stdexcept>
+#include <map>
 
 std::vector<int> Unique(const std::vector<int>& data) {
-    throw std::runtime_error{"Not implemented"};
+    std::vector<int> ans;
+    std::map<int, int> map;
+    for (int i : data) {
+        map.insert(std::pair<int, int>(i, 1));
+    }
+    for (std::map<int, int>::iterator it = map.begin(); it != map.end(); ++it) {
+        ans.push_back(it->first);
+    }
+
+    return ans;
 }
