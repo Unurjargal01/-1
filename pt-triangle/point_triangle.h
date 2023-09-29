23c8f7d29d1be161ae59a9d64bc2264578649e41
Unurjargal01 <ugantumur@edu.hse.ru>
2023-09-29 16:05:41 +0300

    pt_triangle Checked style

--- pt-triangle/point_triangle.h
+++ pt-triangle/point_triangle.h
@@ -10,8 +10,8 @@ struct Triangle {
     Point a, b, c;
 };
 
-bool on_border(const Point& a, const Point& b, const Point& pt) {
-    if (((a.x == pt.x) && (a.y == pt.y)) || ((b.x == pt.x) && (b.y == pt.y)))  {
+bool Onborder(const Point& a, const Point& b, const Point& pt) {
+    if (((a.x == pt.x) && (a.y == pt.y)) || ((b.x == pt.x) && (b.y == pt.y))) {
         return true;
     } else {
         double ct = static_cast<double>(b.x - a.x) / (pt.x - a.x);
@@ -22,24 +22,24 @@ bool on_border(const Point& a, const Point& b, const Point& pt) {
     }
     return false;
 }
-double det(const Point& a, const Point& b) {
+double Det(const Point& a, const Point& b) {
     return static_cast<double>(a.x * b.y - a.y * b.x);
 }
-bool inside(const Point& a, const Point& b, const Point&c, const Point& pt) {
+bool Inside(const Point& a, const Point& b, const Point& c, const Point& pt) {
     Point u{b.x - a.x, b.y - a.y}, v{c.x - a.x, c.y - a.y}, dif{pt.x - a.x, pt.y - a.y};
-    double n = (det(dif, u) - det(a, u)) / det(v, u);
-    double m = - (det(dif, v) - det(a, v)) / det(v, u);
+    double n = (Det(dif, u) - Det(a, u)) / Det(v, u);
+    double m = -(Det(dif, v) - Det(a, v)) / Det(v, u);
 
     if (n > 0 && m > 0 && (n + m < 1)) {
         return true;
-    } 
+    }
     return false;
 }
 bool IsPointInTriangle(const Triangle& t, const Point& pt) {
     Point a1 = t.a, b1 = t.b, c1 = t.c;
-    if (on_border(a1, b1, pt) || on_border(b1, c1, pt) || on_border(c1, a1, pt)) {
+    if (Onborder(a1, b1, pt) || Onborder(b1, c1, pt) || Onborder(c1, a1, pt)) {
         return true;
-    } else if (inside(a1, b1, c1, pt)) {
+    } else if (Inside(a1, b1, c1, pt)) {
         return true;
     }
     return false;
