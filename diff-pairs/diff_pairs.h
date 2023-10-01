6f5bc3c27316e4f7cc08fc1fb878d87d508fb2c2
Unurjargal01 <ugantumur@edu.hse.ru>
2023-10-01 18:46:04 +0300

    Int64_t

--- diff-pairs/diff_pairs.h
+++ diff-pairs/diff_pairs.h
@@ -3,25 +3,21 @@
 #include <vector>
 #include <cstdint>
 #include <stdexcept>
-#include <unordered_map>
+
 #include <map>
 #include <cmath>
 
 int64_t CountPairs(const std::vector<int>& data, int x) {
-    std::map<int, int> counter;
-    int cnt = 0;
-    int int_max = std::pow(2, 31) - 1;
-    for (const int& v : data) {
-        if (((x > 0) && ((x - int_max) > v)) || ((x < 0) && ((v - x) > int_max))) {
-            continue;
+    std::map<int64_t, int> stor;
+    int64_t y = x, diff;
+    int64_t cnt = 0;
+    for (const auto& i : data) {
+        diff = y - i;
+        if (stor.find(diff) != stor.end()) {
+            cnt += stor[diff];
         }
-        int dif = x - v;
-
-        if (counter.find(dif) != counter.end()) {
-            cnt += counter[dif];
-        }
-        if (counter.insert(std::pair<int, int>(v, 1)).second == 0) {
-            counter[v] += 1;
+        if (!stor.insert(std::pair<int, int>(i, 1)).second) {
+            stor[i] += 1;
         }
     }
     return cnt;
