de93972f62c3c9ee8ba2af90ee92faaf3703a05e
Unurjargal01 <ugantumur@edu.hse.ru>
2023-10-03 03:19:30 +0300

    Random Test

--- sort-students/sort_students.h
+++ sort-students/sort_students.h
@@ -3,7 +3,8 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
-
+#include <algorithm>
+#include <ranges>
 struct Student {
     std::string name, surname;
     int day, month, year;
@@ -11,6 +12,29 @@ struct Student {
 
 enum class SortType { kByName, kByDate };
 
+bool CompkByDate(Student& a, Student& b) {
+    if (a.year != b.year) {
+        return (a.year < b.year);
+    } else if (a.month != b.month) {
+        return (a.month < b.month);
+    } else if (a.day != b.day) {
+        return (a.day < b.day);
+    }
+    return true;
+}
+
+bool CompkByName(Student& a, Student& b) {
+    if (a.surname != b.surname) {
+        return a.surname < b.surname;
+    } else if (a.name != b.name) {
+        return a.name < b.name;
+    }
+    return CompkByDate(a, b);
+}
 void SortStudents(std::vector<Student>* students, SortType sort_type) {
-    throw std::runtime_error{"Not implemented"};
+    if (sort_type == SortType::kByDate) {
+        std::ranges::sort(*students, {}, CompkByDate);
+    } else {
+        std::ranges::sort(*students, {}, CompkByName);
+    }
 }
