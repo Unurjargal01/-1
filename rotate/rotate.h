c8781e90cf2092677d7d4ce2fdf4b334cd604ede
Unurjargal01 <ugantumur@edu.hse.ru>
2023-10-04 02:00:49 +0300

    Rotation

--- rotate/rotate.h
+++ rotate/rotate.h
@@ -3,7 +3,20 @@
 #include <stdexcept>
 #include <vector>
 #include <cstddef>
-
+// https://cplusplus.com/reference/algorithm/rotate/
 void Rotate(std::vector<int>* data, size_t shift) {
-    throw std::runtime_error{"Not implemented"};
+    if (shift == 0) {
+        return;
+    }
+    shift %= data->size();
+    size_t first = 0, middle = shift, last = data->size();
+    size_t next = middle;
+    while (first != next) {
+        std::swap(data->at(first++), data->at(next++));
+        if (next == last) {
+            next = middle;
+        } else if (first == middle) {
+            middle = next;
+        }
+    }
 }
