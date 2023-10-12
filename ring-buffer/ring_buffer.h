c5a65a9c64a362b6e2c44095fcce174bf4cad5d5
Unurjargal01 <ugantumur@edu.hse.ru>
2023-10-12 15:53:32 +0300

    Ring Buffer -Ignored Build-asan Memeroy Limit

--- ring-buffer/ring_buffer.h
+++ ring-buffer/ring_buffer.h
@@ -1,25 +1,42 @@
 #pragma once
 
 #include <cstddef>
-
+#include <vector>
+// Implementation without LinkedList
 class RingBuffer {
 public:
-    explicit RingBuffer(size_t capacity) {
+    explicit RingBuffer(size_t capacity) : ring_(capacity), cap_(capacity) {
     }
 
     size_t Size() const {
-        return 0;
+        return size_;
     }
 
     bool Empty() const {
-        return false;
+        return size_ == 0;
     }
 
     bool TryPush(int element) {
+        if (cap_ == size_) {
+            return false;
+        }
+        ++size_;
+        ring_[end_] = element;
+        end_ = (end_ + 1) % cap_;
         return true;
     }
 
     bool TryPop(int* element) {
+        if (!size_) {
+            return false;
+        }
+        *element = ring_[start_];
+        --size_;
+        start_ = (start_ + 1) % cap_;
         return true;
     }
+
+private:
+    std::vector<int> ring_;
+    size_t cap_, size_ = 0, start_ = 0, end_ = 0;
 };
