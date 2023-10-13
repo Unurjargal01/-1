4dc2b477572ba9d1b23908e9fdee33cea9858570
Unurjargal01 <ugantumur@edu.hse.ru>
2023-10-13 18:43:46 +0300

    Fixed Find_last

--- find-last/find_last.h
+++ find-last/find_last.h
@@ -5,9 +5,16 @@
 template <class Iterator, class T>
 Iterator FindLast(Iterator first, Iterator last, const T& value) {
     Iterator ans = last;
-    for (Iterator it = first; it != last; ++it) {
+    // Empty and One element
+    if (first == last) {
+        return last;
+    } else if (first == --last) {
+        return (*first == value) ? first : ans;
+    }
+    for (Iterator it = last; it != first; --it) {
         if (*it == value) {
             ans = it;
+            break;
         }
     }
     return ans;
