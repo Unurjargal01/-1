c194459f9485472db2e839a89516f23eac22090f
Unurjargal01 <ugantumur@edu.hse.ru>
2023-10-13 19:45:52 +0300

    Local Max: Without test case

--- local-max/local_max.h
+++ local-max/local_max.h
@@ -4,5 +4,24 @@
 
 template <class Iterator>
 Iterator LocalMax(Iterator first, Iterator last) {
-    throw std::runtime_error{"Not implemented"};
+    Iterator cur = first;
+    bool left = true;
+    // Empty
+    if (first == last) {
+        return last;
+    } else if (first == --last) {
+        return first;
+    }
+    // Checking first;
+    ++first;
+    ++last;
+    // One element and two elements;
+    for (first; first != last; ++first) {
+        if (left && *cur < *first) {
+            return cur;
+        }
+        left = (*cur < *first);
+        ++cur;
+    }
+    return last;
 }
