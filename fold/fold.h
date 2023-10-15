bab2e7b36b2db0cd05692fab469062bdbdeb8838
Unurjargal01 <ugantumur@edu.hse.ru>
2023-10-15 15:39:40 +0300

    FOLDED

--- fold/fold.h
+++ fold/fold.h
@@ -1,13 +1,45 @@
 #pragma once
 
-struct Sum {};
+#include <vector>
+#include <cstddef>
 
-struct Prod {};
+struct Sum {
+    Sum(){};
+    int operator()(int a, int b) {
+        return a += b;
+    }
+};
 
-struct Concat {};
+struct Prod {
+    Prod(){};
+    int operator()(int a, int b) {
+        return a *= b;
+    }
+};
 
-class Length {};
+struct Concat {
+    Concat(){};
+    auto operator()(auto a, auto b) {
+        a.insert(a.end(), b.begin(), b.end());
+        return a;
+    }
+};
+class Length {
+public:
+    Length(size_t* cnt) : ans_(cnt){};
+    auto operator()(auto a, auto) {
+        ++(*ans_);
+        return a;
+    }
+
+private:
+    size_t* ans_;
+};
 
 template <class Iterator, class T, class BinaryOp>
 T Fold(Iterator first, Iterator last, T init, BinaryOp func) {
+    for (; first != last; ++first) {
+        init = func(init, *first);
+    }
+    return init;
 }
