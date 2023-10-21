4e1877ed3525720c304ed499cf80b0af0357dfe9
Unurjargal01 <ugantumur@edu.hse.ru>
2023-10-21 13:10:08 +0300

    Raytracer Geom

--- raytracer-geom/geometry.h
+++ raytracer-geom/geometry.h
@@ -5,12 +5,111 @@
 #include <intersection.h>
 #include <triangle.h>
 #include <ray.h>
-
+#include <cmath>
 #include <optional>
 
-std::optional<Intersection> GetIntersection(const Ray& ray, const Sphere& sphere);
-std::optional<Intersection> GetIntersection(const Ray& ray, const Triangle& triangle);
+auto SolveQuad(const double& a, const double& b, const double& c) {
+    double disc = b * b - 4 * a * c;
+    double x0, x1;
+    if (disc == 0) {
+        x0 = x1 = -b / (2 * a);
+    } else {
+        double q = (b > 0) ? -(b + std::sqrt(disc)) / 2 : -(b - std::sqrt(disc)) / 2;
+        x0 = q / a;
+        x1 = c / q;
+    }
+    if (x0 > x1) {
+        std::swap(x0, x1);
+    }
+    if (x0 > 0) {
+        return x0;
+    }
+    return (x1 > 0) ? x1 : -1;
+}
+bool InsideCircle(const Vector& v, const Sphere& s) {
+    return (Length(s.GetCenter() - v) <= s.GetRadius());
+}
+std::optional<Intersection> GetIntersection(const Ray& ray, const Sphere& sphere) {
+    Vector oc = ray.GetOrigin() - sphere.GetCenter();
+    double b = 2 * DotProduct(ray.GetDirection(), oc);
+    double c = DotProduct(oc, oc) - sphere.GetRadius() * sphere.GetRadius();
+    double t0 = SolveQuad(1, b, c);
+    if (t0 < 0) {
+        return std::nullopt;
+    }
+    Vector pos = ray.GetOrigin() + t0 * ray.GetDirection();
+    // Check inside or outside;
+    Vector norm = pos - sphere.GetCenter();
+    if (InsideCircle(ray.GetOrigin(), sphere)) {
+        norm *= -1;
+    }
+    norm.Normalize();
+    return Intersection{pos, norm, std::abs(t0)};
+};
+std::optional<Intersection> GetIntersection(const Ray& ray, const Triangle& triangle) {
+    const double eps = 0.000001;
+    Vector v0 = triangle[0], v1 = triangle[1], v2 = triangle[2];
+    Vector edge1, edge2, h, s, q;
+    double a, f, u, v;
+    edge1 = v1 - v0;
+    edge2 = v2 - v0;
+    h = CrossProduct(ray.GetDirection(), edge2);
+    a = DotProduct(edge1, h);
 
-Vector Reflect(const Vector& ray, const Vector& normal);
-std::optional<Vector> Refract(const Vector& ray, const Vector& normal, double eta);
-Vector GetBarycentricCoords(const Triangle& triangle, const Vector& point);
+    if (a > -eps && a < eps) {
+        return std::nullopt;
+    }
+    f = 1.0 / a;
+    s = ray.GetOrigin() - v0;
+    u = f * DotProduct(s, h);
+
+    if (u < 0.0 || u > 1.0) {
+        return std::nullopt;
+    }
+    q = CrossProduct(s, edge1);
+    v = f * DotProduct(ray.GetDirection(), q);
+
+    if (v < 0.0 || u + v > 1.0) {
+        return std::nullopt;
+    }
+
+    double t = f * DotProduct(edge2, q);
+
+    Vector norm = CrossProduct(edge1, edge2);
+    norm.Normalize();
+    if (DotProduct(norm, ray.GetDirection()) > 0) {
+        norm *= -1;
+    }
+    if (t > eps) {
+        // here possibility is that normal might have wrong direction;
+        return Intersection{ray.GetOrigin() + ray.GetDirection() * t, norm, t};
+    }
+    return std::nullopt;
+};
+Vector Reflect(const Vector& ray, const Vector& normal) {
+    Vector ans = 2 * (DotProduct(normal, ray)) * normal - ray;
+    return -1 * ans;
+};
+std::optional<Vector> Refract(const Vector& ray, const Vector& normal, double eta) {
+    // eta > 1;
+    double c = -DotProduct(normal, ray);
+    double int_ref = 1 - eta * eta * (1 - c * c);
+    if (int_ref < 0) {
+        return std::nullopt;
+    }
+    Vector ans = eta * ray + (eta * c - std::sqrt(int_ref)) * normal;
+    return ans;
+};
+
+Vector GetBarycentricCoords(const Triangle& triangle, const Vector& point) {
+    Vector v0 = triangle[0], v1 = triangle[1], v2 = triangle[2];
+    double area = Length(CrossProduct(v1 - v0, v2 - v0));
+    double a = Length(CrossProduct(v1 - v2, v1 - point)) / area;
+    double b = Length(CrossProduct(v0 - v2, v2 - point)) / area;
+    double c = Length(CrossProduct(v1 - v0, v0 - point)) / area;
+
+    if (a >= 0 && b >= 0 && 1 >= a + b) {
+        return Vector{a, b, c};
+    }
+    return Vector();
+}
--- raytracer-geom/vector.h
+++ raytracer-geom/vector.h
@@ -2,21 +2,91 @@
 
 #include <array>
 #include <cstddef>
+#include <cmath>
 
 class Vector {
 public:
-    Vector();
-    Vector(double x, double y, double z);
+    // why need to initiate empty constructor;
+    Vector(){};
+    Vector(double x, double y, double z) {
+        data_[0] = x;
+        data_[1] = y;
+        data_[2] = z;
+    }
+    double& operator[](size_t ind) {
+        return data_[ind];
+    };
+    double operator[](size_t ind) const {
+        return data_[ind];
+    };
 
-    double& operator[](size_t ind);
-    double operator[](size_t ind) const;
+    double Length() const {
+        double ans = 0;
+        for (size_t i = 0; i < 3; ++i) {
+            ans += data_[i] * data_[i];
+        }
+        return std::sqrt(ans);
+    }
 
-    void Normalize();
+    void Normalize() {
+        double len = this->Length();
+        for (auto& i : data_) {
+            i /= len;
+        }
+    };
+    // Extra operations
+    Vector& operator+=(const Vector& rhs) {
+        data_[0] += rhs[0];
+        data_[1] += rhs[1];
+        data_[2] += rhs[2];
+        return *this;
+    }
+
+    Vector& operator*=(const double& coef) {
+        data_[0] *= coef;
+        data_[1] *= coef;
+        data_[2] *= coef;
+        return *this;
+    }
+    friend const Vector operator+(const Vector& lhs, const Vector& rhs) {
+        return Vector{lhs[0] + rhs[0], lhs[1] + rhs[1], lhs[2] + rhs[2]};
+    }
+    friend const Vector operator-(const Vector& lhs, const Vector& rhs) {
+        return Vector{lhs[0] - rhs[0], lhs[1] - rhs[1], lhs[2] - rhs[2]};
+    }
+
+    friend const Vector operator*(const Vector& lhs, const double& coef) {
+        return Vector{lhs[0] * coef, lhs[1] * coef, lhs[2] * coef};
+    }
+    friend const Vector operator*(const double& coef, const Vector& lhs) {
+        return Vector{lhs[0] * coef, lhs[1] * coef, lhs[2] * coef};
+    }
+    Vector& operator/(const double nom) {
+        data_[0] /= nom;
+        data_[1] /= nom;
+        data_[2] /= nom;
+        return *this;
+    }
+    // Just in case: rule of 3
 
 private:
     std::array<double, 3> data_;
 };
 
-double DotProduct(const Vector& a, const Vector& b);
-Vector CrossProduct(const Vector& a, const Vector& b);
-double Length(const Vector& v);
+double DotProduct(const Vector& a, const Vector& b) {
+    double ans = 0;
+    for (int i = 0; i < 3; ++i) {
+        ans += a[i] * b[i];
+    }
+    return ans;
+};
+Vector CrossProduct(const Vector& a, const Vector& b) {
+    double x, y, z;
+    x = a[1] * b[2] - a[2] * b[1];
+    y = -(a[0] * b[2] - a[2] * b[0]);
+    z = a[0] * b[1] - a[1] * b[0];
+    return Vector{x, y, z};
+}
+double Length(const Vector& v) {
+    return std::sqrt(DotProduct(v, v));
+};
