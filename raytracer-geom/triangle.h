c8b648cf6360a352675e655142176eb6e67c849e
Unurjargal01 <ugantumur@edu.hse.ru>
2023-10-21 13:10:08 +0300

    Raytracer Geom

--- raytracer-geom/triangle.h
+++ raytracer-geom/triangle.h
@@ -1,11 +1,22 @@
 #pragma once
 
 #include <vector.h>
-
+#include <array>
 class Triangle {
 public:
-    Triangle(const Vector& a, const Vector& b, const Vector& c);
+    Triangle(const Vector& a, const Vector& b, const Vector& c) {
+        data_[0] = a;
+        data_[1] = b;
+        data_[2] = c;
+    };
 
-    const Vector& operator[](size_t ind) const;
-    double Area() const;
+    const Vector& operator[](size_t ind) const {
+        return data_[ind];
+    };
+    double Area() const {
+        return Length(CrossProduct(data_[1] - data_[0], data_[2] - data_[0])) / 2;
+    };
+
+private:
+    std::array<Vector, 3> data_;
 };
