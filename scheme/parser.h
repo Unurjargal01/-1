1808e603ccd98f86de8b3b246a80ec96a9161949
ugantumur@edu.hse.ru <ugantumur@edu.hse.ru>
2023-11-23 21:16:47 +0300

    Parser without std::cout

--- scheme/parser.h
+++ scheme/parser.h
@@ -37,7 +37,7 @@ std::shared_ptr<Object> Read(Tokenizer* tokenizer) {
 
 // ReadAny;
 std::shared_ptr<Object> ReadCell(Tokenizer* tokenizer) {
-    std::cout << "Read Cell Started: " << std::endl;
+    // std::cout << "Read Cell Started: " << std::endl;
     std::shared_ptr<Object> current, next;
     // bool current_state = tokenizer->IsEnd();
     Token cur_token = tokenizer->GetToken();
@@ -46,10 +46,10 @@ std::shared_ptr<Object> ReadCell(Tokenizer* tokenizer) {
     // Current, ?? if it is our guess is right, we shouldn't encounter the dot in current state.
     if (std::holds_alternative<ConstantToken>(cur_token)) {
         current = std::make_shared<Number>(std::get<ConstantToken>(cur_token).value);
-        std::cout << std::get<ConstantToken>(cur_token).value << std::endl;
+        // std::cout << std::get<ConstantToken>(cur_token).value << std::endl;
     } else if (std::holds_alternative<SymbolToken>(cur_token)) {
         current = std::make_shared<Symbol>(std::get<SymbolToken>(cur_token).name);
-        std::cout << std::get<SymbolToken>(cur_token).name << std::endl;
+        // std::cout << std::get<SymbolToken>(cur_token).name << std::endl;
     } else if (std::holds_alternative<BracketToken>(cur_token)) {
         auto bracket = std::get<BracketToken>(cur_token);
         if (bracket == BracketToken::OPEN) {
@@ -59,16 +59,16 @@ std::shared_ptr<Object> ReadCell(Tokenizer* tokenizer) {
             next_token = tokenizer->GetToken();
         } else {
             current = std::make_shared<BracketClosed>();
-            std::cout << "Read: BrackedClosed" << std::endl;
+            // std::cout << "Read: BrackedClosed" << std::endl;
             return current;
         }
     } else if (std::holds_alternative<DotToken>(cur_token)) {
-        std::cout << "FAILED: DotToken" << std::endl;
+        // std::cout << "FAILED: DotToken" << std::endl;
         throw SyntaxError();
     }
     // Next;
     if (std::holds_alternative<DotToken>(next_token)) {
-        std::cout << "Read: DOTTOKEN" << std::endl;
+        // std::cout << "Read: DOTTOKEN" << std::endl;
         tokenizer->Next();
         next_token = tokenizer->GetToken();
         if (std::holds_alternative<ConstantToken>(next_token)) {
@@ -83,28 +83,28 @@ std::shared_ptr<Object> ReadCell(Tokenizer* tokenizer) {
                 tokenizer->Next();
                 next = ReadList(tokenizer);
             } else {
-                std::cout << "Failed: Next ClosedBracket" << std::endl;
+                // std::cout << "Failed: Next ClosedBracket" << std::endl;
                 throw SyntaxError();
             }
         } else {
-            std::cout << "FAILED: Next" << std::endl;
+            // std::cout << "FAILED: Next" << std::endl;
             throw SyntaxError();
         }
         // Here Careful
     }
     if (next != nullptr) {
-        std::cout << "Dot Sucessed" << std::endl;
+        // std::cout << "Dot Sucessed" << std::endl;
         current = std::make_shared<Cell>(current, next);
     } else if (Is<Cell>(current)) {  // Change is here.
-        std::cout << "FAILED CELL" << std::endl;
+        // std::cout << "FAILED CELL" << std::endl;
         current = std::make_shared<Cell>(current, nullptr);
     }
-    std::cout << "ReadCell finished: " << std::endl;
+    // std::cout << "ReadCell finished: " << std::endl;
     return current;
 }
 
 std::shared_ptr<Object> ReadList(Tokenizer* tokenizer) {
-    std::cout << "ReadList started: " << std::endl;
+    // std::cout << "ReadList started: " << std::endl;
     std::shared_ptr<Cell> ans;
     std::shared_ptr<Cell> ptr_second;
     std::shared_ptr<Object> elem = ReadCell(tokenizer);
@@ -112,13 +112,13 @@ std::shared_ptr<Object> ReadList(Tokenizer* tokenizer) {
     while (!Is<BracketClosed>(elem)) {
         // Maybe the last element is bracket?
         if (tokenizer->IsEnd()) {
-            std::cout << "FAILED: FINISHED" << std::endl;
+            // std::cout << "FAILED: FINISHED" << std::endl;
             throw SyntaxError();
         }
         if (ans == nullptr) {
             if (Is<Number>(elem) || Is<Symbol>(elem)) {
                 ans = std::make_shared<Cell>(elem, nullptr);
-                std::cout << "Assigned: " << std::endl;
+                // std::cout << "Assigned: " << std::endl;
             } else if (Is<Cell>(elem)) {
                 ans = As<Cell>(elem);
                 // We can break here if we want;
@@ -130,17 +130,17 @@ std::shared_ptr<Object> ReadList(Tokenizer* tokenizer) {
                 ptr_second = As<Cell>(ptr_second->second_);
             } else if (Is<Cell>(elem)) {
                 // Our Guess;
-                std::cout << "FAILED GUESS" << std::endl;
+                // std::cout << "FAILED GUESS" << std::endl;
                 ptr_second->second_ = elem;
                 ptr_second = As<Cell>(ptr_second->second_);
                 // What if it is not Cell the second part
             }
         } else {
-            std::cout << "FAILED: Assignment while" << std::endl;
+            // std::cout << "FAILED: Assignment while" << std::endl;
             throw SyntaxError();
         }
         elem = ReadCell(tokenizer);
     };
-    std::cout << "ReadList finished: " << std::endl;
+    // std::cout << "ReadList finished: " << std::endl;
     return ans;
 }
